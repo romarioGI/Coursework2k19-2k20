d96b5c1b8fc0435ecb1ab12a4141fd9fcf9f9d8a
romarioGI <roma.fenc@yandex.ru>
2020-04-19 21:11:30 +0300

    saturation of the polynomial system

---
+++ TarskiAlgorithm/MathLib/PolynomialsDegreeComparator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MathLib
+{
+    public class PolynomialsDegreeComparator<T> : IComparer<Polynomial<T>> where T : AbstractNumber
+    {
+        public int Compare(Polynomial<T> x, Polynomial<T> y)
+        {
+            if(x is null || y is null)
+                throw new ArgumentNullException();
+
+            return x.Degree.CompareTo(y.Degree);
+        }
+    }
+}
