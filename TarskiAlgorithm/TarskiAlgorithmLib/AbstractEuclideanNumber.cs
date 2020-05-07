0a682be85f148a08222645ac76430d5d27f8e1f7
romarioGI <roma.fenc@yandex.ru>
2020-05-07 18:32:06 +0300

    abstract structures

---
+++ TarskiAlgorithm/TarskiAlgorithmLib/AbstractEuclideanNumber.cs
@@ -0,0 +1,21 @@
+﻿namespace TarskiAlgorithmLib
+{
+    public abstract class AbstractEuclideanNumber : AbstractNumber
+    {
+        public static AbstractNumber GreatestCommonDivisor(AbstractEuclideanNumber first,
+            AbstractEuclideanNumber second)
+        {
+            var firstNum = (AbstractNumber) first;
+            var secondNum = (AbstractNumber) second;
+            while (!secondNum.IsZero)
+            {
+                firstNum %= secondNum;
+                var temp = firstNum;
+                firstNum = secondNum;
+                secondNum = temp;
+            }
+
+            return firstNum;
+        }
+    }
+}
---
+++ TarskiAlgorithm/TarskiAlgorithmLib/Exceptions/NumberTypeException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TarskiAlgorithmLib.Exceptions
+{
+    internal class NumberTypeException : Exception
+    {
+        public NumberTypeException(AbstractNumber first, AbstractNumber second) : base(GetMessage(first, second))
+        {
+        }
+
+        private static string GetMessage(AbstractNumber first, AbstractNumber second)
+        {
+            return $"{first.GetType()} and {second.GetType()} are incompatible AbstractNumber types";
+        }
+    }
+}
